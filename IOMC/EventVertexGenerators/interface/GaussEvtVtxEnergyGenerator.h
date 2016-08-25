75003026bf636080cd29c7e80ce5f761c8827663
Michał Sadowski <sadowskimichal95@gmail.com>
2016-08-25 15:21:28 +0200

    add beam smearing

--- IOMC/EventVertexGenerators/interface/GaussEvtVtxEnergyGenerator.h
+++ IOMC/EventVertexGenerators/interface/GaussEvtVtxEnergyGenerator.h
@@ -3,19 +3,33 @@
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Utilities/interface/InputTag.h"
+#include "IOMC/EventVertexGenerators/interface/GaussEvtVtxGenerator.h"
+#include "HepMC/GenEvent.h"
+
+
 
 class GaussEvtVtxEnergyGenerator : public edm::EDProducer{
 public:
     GaussEvtVtxEnergyGenerator(const edm::ParameterSet &p);
     virtual ~GaussEvtVtxEnergyGenerator();
     virtual void produce( edm::Event&, const edm::EventSetup&);
+    void applyBeamSmearing(const HepMC::GenEvent *evt, CLHEP::HepRandomEngine* engine);
+    void beginRun(edm::Run const&iR, edm::EventSetup const&es);
+    HepMC::FourVector* newVertex(CLHEP::HepRandomEngine* engine);
 
 private:
-    double fSigmaX, fSigmaY, fSigmaZ;
-    double fMeanX,  fMeanY,  fMeanZ;
-    double fTimeOffset;
-    edm::InputTag sourceLabel;
 
+    double MeanXi, SiXi;                            ///< Xi distribution
+    double Al;                                      ///< Alpha (half crossing angle) d.
+    double SiThX, SiThY;                            ///< Theta (beam divergence) d.
+
+    double E_CMS;
+
+    double MeanX, MeanY, MeanZ;                     ///< [mm] means
+    double SiX, SiY, SiZ;                           ///< [mm] variance
+
+    edm::InputTag sourceLabel;
+    unsigned int verbosity;
 };
 
 #endif
